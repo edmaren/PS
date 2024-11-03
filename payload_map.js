e8f401a6314ec2e1a5511ddfb1088780d7e8d12b
edmaren <134834112+edmaren@users.noreply.github.com>
2024-11-04 00:53:24 +0100

    Add files via upload

---
+++ payload_map.js
@@ -0,0 +1,12 @@
+const payload_map =
+    [
+        {
+            displayTitle: 'Versions',
+            description: 'Shows kernel build, os and sdk versions',
+            fileName: 'versions.elf',
+            author: 'SiSTRo',
+            source:'https://github.com/SiSTR0/ps5-versions/releases/download/v1.0/ps5-versions.elf',
+            version: '1.0'
+        }
+
+    ];
